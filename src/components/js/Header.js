1c0d307b1359d8cda5704b3b246b3a6769396d02
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-04-20 12:46:07 -0400

    added header component and background image

---
+++ src/components/js/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>The Planets</h1>
+      <ul>
+        <li>mars</li>
+      </ul>
+    </header>
+  )
+}
