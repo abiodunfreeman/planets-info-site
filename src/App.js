1fbeacc8306504f4e5856499b8557a1d1195d8f1
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-04-21 12:24:47 -0400

    css changes, conditional class added to <Main/>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/js/Header';
 import Main from './components/js/Main';
 import data from "./data.json"
 function App() {
-  
+  const [curPlanet, setCurPlanet] = useState(data[0])
   return (
     <div className="App">
       <Header data={data}/>
-      <Main data={data}/>
+      <Main data={data} planet={curPlanet}/>
     </div>
   );
 }
--- src/components/js/Main.js
+++ src/components/js/Main.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import "../css/main.css"
 export default function Main(props) {
-  const {data} = props
+  const {data, planet} = props
   let primaryColor;
-  
+  console.log(planet.name)
+ 
   if (window.innerWidth <= 450){
      return (
-      <div id="main">
+      <div id="main" className={planet.name}>
         <div id="tabs">
           <h3>OVERVIEW</h3>
           <h3>STRUCTURE</h3>
           <h3>SURFACE</h3>
         </div>
-       
+       <img
+        src={planet.images.planet}
+        />
+        <div className="main--desc">
+          <h1>{planet.name}</h1>
+          <p>{planet.overview.content}</p>
+          <p>Source: <a target="_blank" href={planet.overview.source}>Wikipedia</a></p>
+        </div>
       </div>
     )
   } else if (window.innerWidth <= 800){
